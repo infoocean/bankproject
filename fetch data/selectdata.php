5c52e58796d207fe7a851147fe967a26c9e93e5b
shubham kumar jaiswal <63592223+infoocean@users.noreply.github.com>
2021-03-09 14:34:00 +0530

    add title

--- fetch data/selectdata.php
+++ fetch data/selectdata.php
@@ -37,7 +37,7 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>details</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
